abe2b574ca36bb400b5fb6c8251baf9df6987243
soonode97 <soonode951@gmail.com>
2024-12-10 16:51:18 +0900

    fix: 메인서버 프로토 작업
    1. 메인서버 프로토 최신화
    2. 안쓰는 패킷들 제거

--- packages/modules/constants/packet/client.packet.js
+++ packages/modules/constants/packet/client.packet.js
@@ -4,25 +4,18 @@ const CLIENT_PACKET = {
     LoginResponse: 17, // 응답 성공하면 로비에 진입
   },
   lobby: {
-    EnterLobbyRequest: 501, // 로비 진입 요청
-    EnterLobbyResponse: 502, // 로비 진입 성공 응답 - 대기실 리스트 정보를 불러와서 보내줌
-    UpdateSettingRequest: 503, // 설정 변경 요청
-    UpdateSettingResponse: 504, // 설정 변경 저장에 따른 응답 - 추후 프리셋 / 롤백에 대비하여 미리 세팅값 저장
-    EnterListOfRoomRequest: 505, // 대기실 리스트 진입 요청
-    EnterListOfRoomResponse: 506, // 대기실 리스트 진입 성공 시 응답, 대기방 목록 리스트 보냄
-    RefreshListOfRoomRequest: 507, // 대기실 리스트 새로고침 요청
-    RefreshListOfRoomResponse: 508, // 대기실 리스트 새로고침 응답
-    FilterRoomByHostUserRequest: 509, // 대기실 리스트 호스트 유저 필터 요청
-    FilterRoomByHostUserResponse: 510, // 대기실 리스트호스트 유저 필터 응답, 해당 유저의 대기방 정보 보냄
-    SearchRoomRequest: 511, // 대기실 리스트 검색 요청
-    SearchRoomResponse: 512, // 대기실 리스트 검색 응답
-    // 정렬은 추후 고려,
-    CreateWaitingRoomRequest: 513, // 대기방 생성 요청
-    CreateWaitingRoomResponse: 514, // 대기방 생성 응답
-    CreateWaitingRoomNotification: 515, // 대기방 생성 알림 (전체 클라, 단 로비 서버에 접속해있는 유저만)
-    JoinWaitingRoomRequest: 516, // 대기방 참가 요청
-    JoinWaitingRoomResponse: 517, // 대기방 참가 응답, 해당 대기방에 속한 유저 정보 보냄
-    // 이후 추가 필요
+    RegistAccountRequest: 500,
+    RegistAccountResponse: 501,
+    LoginRequest: 502,
+    LoginResponse: 503,
+    ChangeNicknameRequest: 504,
+    ChangeNicknameResponse: 505,
+    EnterLobbyRequest: 506,
+    EnterLobbyResponse: 507,
+    RefreshLobbyRequest: 508,
+    RefreshLobbyResponse: 509,
+    SearchRoomRequest: 510,
+    SearchRoomResponse: 511,
   },
 };
 
--- packages/modules/constants/protoNames/client.proto.js
+++ packages/modules/constants/protoNames/client.proto.js
@@ -1,30 +1,18 @@
 import clientPacket from '@peekaboo-ssr/modules-constants/clientPacket';
 
 const CLIENT_PACKET_MAPS = {
-  [clientPacket.account.CreateUserRequest]: 'createUserRequest',
-  [clientPacket.account.CreateUserResponse]: 'createUserResponse',
+  [clientPacket.account.RegistAccountRequest]: 'registAccountRequest',
+  [clientPacket.account.RegistAccountResponse]: 'registAccountResponse',
   [clientPacket.account.LoginRequest]: 'loginRequest',
   [clientPacket.account.LoginResponse]: 'loginResponse',
+  [clientPacket.account.ChangeNicknameRequest]: 'changeNicknameRequest',
+  [clientPacket.account.ChangeNicknameResponse]: 'changeNicknameResponse',
   [clientPacket.lobby.EnterLobbyRequest]: 'enterLobbyRequest',
   [clientPacket.lobby.EnterLobbyResponse]: 'enterLobbyResponse',
-  [clientPacket.lobby.UpdateSettingRequest]: 'updateSettingRequest',
-  [clientPacket.lobby.UpdateSettingResponse]: 'updateSettingResponse',
-  [clientPacket.lobby.EnterListOfRoomRequest]: 'enterListOfRoomRequest',
-  [clientPacket.lobby.EnterListOfRoomResponse]: 'enterListOfRoomResponse',
-  [clientPacket.lobby.RefreshListOfRoomRequest]: 'refreshListOfRoomRequest',
-  [clientPacket.lobby.RefreshListOfRoomResponse]: 'refreshListOfRoomResponse',
-  [clientPacket.lobby.FilterRoomByHostUserRequest]:
-    'filterRoomByHostUserRequest',
-  [clientPacket.lobby.FilterRoomByHostUserResponse]:
-    'filterRoomByHostUserResponse',
+  [clientPacket.lobby.RefreshLobbyRequest]: 'refreshLobbyRequest',
+  [clientPacket.lobby.RefreshLobbyResponse]: 'refreshLobbyResponse',
   [clientPacket.lobby.SearchRoomRequest]: 'searchRoomRequest',
   [clientPacket.lobby.SearchRoomResponse]: 'searchRoomResponse',
-  [clientPacket.lobby.CreateWaitingRoomRequest]: 'createWaitingRoomRequest',
-  [clientPacket.lobby.CreateWaitingRoomResponse]: 'createWaitingRoomResponse',
-  [clientPacket.lobby.CreateWaitingRoomNotification]:
-    'createWaitingRoomNotification',
-  [clientPacket.lobby.JoinWaitingRoomRequest]: 'joinWaitingRoomRequest',
-  [clientPacket.lobby.JoinWaitingRoomResponse]: 'joinWaitingRoomResponse',
 };
 
 export default CLIENT_PACKET_MAPS;
